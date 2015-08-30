6bfc4d103f69af449704cfc4a137fa5c154dc595
dfenjves <fenjves.daniel@gmail.com>
2015-08-29 22:52:16 -0400

    solved this thing

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -12,10 +12,28 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+-(NSArray *)arrayBySortingArrayAscending:(NSArray *)argumentArray;
+
+-(NSArray *)arrayBySortingArrayDescending:(NSArray *)argumentArray;
+
+-(NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)argumentArray;
+
+-(NSArray *)arrayByReversingArray:(NSArray *)argumentArray;
+
+-(NSString *)stringInBasicLeetFromString:(NSString *)argumentString;
+
+-(NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)argumentArray;
+
+-(NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)argumentDictionary;
+
+-(NSArray *)stringsBeginningWithAInArray:(NSArray *)argumentArray;
+
+-(NSInteger)sumOfIntegersInArray:(NSArray *)argumentArray;
+
+-(NSArray *)arrayByPluralizingStringsInArray:(NSArray *)argumentArray;
+
+-(NSDictionary *)countsOfWordsInString:(NSString *)argumentString;
+
+-(NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)argumentArray;
 
 @end
